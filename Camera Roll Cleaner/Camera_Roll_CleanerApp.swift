77615e75f3d8569fc50c89a98152e434975917e5
Michael Hines <hines45@gmail.com>
2026-09-01 22:45:11 +0200

    Initial Commit

---
+++ Camera Roll Cleaner/Camera_Roll_CleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Camera_Roll_CleanerApp.swift
+//  Camera Roll Cleaner
+//
+//  Created by Michael Hines on 01.09.26.
+//
+
+import SwiftUI
+
+@main
+struct Camera_Roll_CleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Camera Roll Cleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Camera Roll Cleaner
+//
+//  Created by Michael Hines on 01.09.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
